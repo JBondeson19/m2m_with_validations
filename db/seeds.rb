1d71987a7d47f8c7752dbb9a9714360619b4f276
raaynaldo <raaynaldo@hotmail.com>
2020-11-03 17:26:15 -0500

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,6 @@ BakeShop.destroy_all
 Good.destroy_all
 
 bake_shops = []
-
 10.times do
   bake_shops << BakeShop.create(name: Faker::Games::Pokemon.move + " Bake Shop",
                                 city: Faker::Games::DnD.city,
@@ -17,8 +16,8 @@ bake_shops = []
 end
 
 50.times do
-  Good.create(name: Faker::Dessert.variety,
-              price: rand(1.00...100.00),
+  Good.create(name: Faker::Dessert.flavor + " " + Faker::Dessert.variety,
+              price: rand(1.00...100.00).round(2),
               calories: rand(50...500),
               bake_shop_id: bake_shops.sample.id)
 end
