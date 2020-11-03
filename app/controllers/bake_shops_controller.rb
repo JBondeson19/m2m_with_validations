27297eb80b8f51f99ef415c10018246add396568
JBondeson19 <jpbond58@gmail.com>
2020-11-03 15:21:24 -0500

    "crud"

--- app/controllers/bake_shops_controller.rb
+++ app/controllers/bake_shops_controller.rb
@@ -1,6 +1,6 @@
 class BakeShopsController < ApplicationController
   
-    before_action :bake_shop_id, only: [:show]
+    before_action :bake_shop_id, only: [:show, :destroy, :edit, :update]
 
     def index 
         @bake_shops = BakeShop.all
@@ -9,11 +9,28 @@ class BakeShopsController < ApplicationController
     def show
     end
 
+    def new 
+        @bake_shop = BakeShop.new
+    end
+
+    def create
+        bake_shop = BakeShop.create(get_params)
+        redirect_to bake_shop_path(bake_shop)
+    end
+
+    def edit 
+    end
+
+    def update
+        @bake_shop.update(get_params)
+        redirect_to bake_shop_path(@bake_shop)
+    end
 
 
-
-
-
+    def destroy
+        @bake_shop.destroy
+        redirect_to bake_shops_path
+    end
 
 
     private
