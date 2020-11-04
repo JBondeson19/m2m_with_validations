c04eccf30c71bfa4563eff55253d106fdb4354ce
JBondeson19 <jpbond58@gmail.com>
2020-11-04 13:49:17 -0500

    bakeshop errors

--- app/models/bake_shop.rb
+++ app/models/bake_shop.rb
@@ -2,5 +2,5 @@ class BakeShop < ApplicationRecord
     has_many :goods
     validates :name, presence: true, uniqueness: true 
     validates :city, presence: true
-    validates :phone_number, length: {minimum: 13}
+    validates_format_of :phone_number, :with =>  /\d[0-9]\)*\z/ , :message => "Only positive number without spaces are allowed"
 end
